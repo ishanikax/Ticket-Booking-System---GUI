d85f4662b47f740cd98a3d4f0e69bb5dc85c50ed
Ishanika <ishanikas04@gmail.com>
2024-05-14 19:09:24 +1200

    First Commit

---
+++ src/project2_code/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project2_code;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Ishanika
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+
+        MyFrame myFrame = new MyFrame();
+        JLabel label = new JLabel();
+
+        
+
+    }    
+
+}
